d1ff539f9074ea16f274dceb932666aa83e04238
Caitlyn McAllister <Caitlyn.Mcallister@noaa.gov>
2025-03-28 17:45:03 +0000

    edit build_tracker.sh to include compilation of gfdl tracker src code, then copy gfdl executables and move them into tc_tracker exec/ dir

--- sorc/build_tracker.sh
+++ sorc/build_tracker.sh
@@ -31,5 +31,27 @@ make -j 8 VERBOSE=2
 make install
 
 cd ..
+export home=$PWD
 
+# move into sorc/gfdl_tracker.fd directory
+export gfdl_tracker=sorc/gfdl_tracker.fd/code/
+cd $gfdl_tracker
+
+# build & compile gfdl-vortextracker executables
+if [ -d "build" ]; then
+   rm -rf build
+fi
+mkdir build && cd build
+cmake .. -DCMAKE_Fortran_COMPILER=${CMAKE_Fortran_COMPILER} -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
+make
+make install
+
+# copy gfdl-tracker executables & move them into tc_tracker exec/ dir
+cd ..
+export gfdl_exec=exec/
+cd $gfdl_exec
+cp * $home/exec/.
+
+# move back into home/base directory
+cd $home
 exit
