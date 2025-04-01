3f48fd6f179fcce940cb18aeb4a552d1ccbd538e
Caitlyn McAllister <Caitlyn.Mcallister@noaa.gov>
2025-04-01 17:16:13 +0000

    edit build_tracker.sh to clone, build, & compile GFDL-VortexTracker

--- sorc/build_tracker.sh
+++ sorc/build_tracker.sh
@@ -31,5 +31,32 @@ make -j 8 VERBOSE=2
 make install
 
 cd ..
+export basedir=$PWD
+export sorcdir=$basedir/sorc
 
+# clone gfdl vortex tracker in sorc directory
+cd $sorcdir
+git clone https://github.com/NOAA-GFDL/GFDL-VortexTracker.git gfdl_tracker.fd
+
+# move into gfdl_tracker.fd directory
+export gfdl_tracker=gfdl_tracker.fd/code/
+cd $gfdl_tracker
+
+# build & compile gfdl-vortextracker executables
+if [ -d "build" ]; then
+   rm -rf build
+fi
+mkdir build && cd build
+cmake .. -DCMAKE_Fortran_COMPILER=${CMAKE_Fortran_COMPILER} -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
+make
+make install
+
+# copy gfdl-tracker executables & move them into tc_tracker exec/ dir
+cd ..
+export gfdl_exec=exec/
+cd $gfdl_exec
+cp * $basedir/exec/.
+
+# move back into home/base directory
+cd $basedir
 exit
